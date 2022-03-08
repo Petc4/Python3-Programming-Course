fcc1f19d55bd266d0eb811419b92486c2a1a8235
Petc4 <101171618+Petc4@users.noreply.github.com>
2022-03-08 15:54:59 +0700

    Create Exercise4_Dollapak_S.py

---
+++ Exercise4_Dollapak_S.py
@@ -0,0 +1,9 @@
+FE  = 80.5
+GB  = 75.5
+ICS = 84
+CP  = 75.5
+print("--- Your Score ---")
+print("Foundation English", FE)
+print("General Business", GB)
+print("Irtroduction to Computer Systems", ICS)
+print("Computer Programming", CP)
