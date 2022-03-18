c76525f87c60f5e819d40e5d8a828c79fb9929ce
Petc4 <101171618+Petc4@users.noreply.github.com>
2022-03-18 11:31:18 +0700

    Create Lecture50_Dollapak_S.py

---
+++ Lecture50_Dollapak_S.py
@@ -0,0 +1,8 @@
+def addnumber(x, y):
+    print("x", "+", "y", "=", x + y)
+    print("x", "-", "y", "=", x - y)
+    print("x", "*", "y", "=", x * y)
+    print("x", "/", "y", "=", x / y)
+x = int(input("please number number : "))
+y = int(input("please number number : "))
+addnumber(x,y)
